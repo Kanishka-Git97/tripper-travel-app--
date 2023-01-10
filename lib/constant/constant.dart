2e066a5ddbd5c35b0f71244d2213cbb660b46272
Sandini Kaveesha <sandinikaveesha@gmail.com>
2023-01-10 23:40:04 +0530

    created constant file

---
+++ lib/constant/constant.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+// Colors
+const Color primaryColor = Color(0xff2687A4);
+const Color secondaryColor = Color(0xffF7F9FA);
+const Color bgColor = Color(0xffF9F9F9);
+const Color bgCard = Colors.white;
+const Color tabBar = Color(0xffE9EFF0);
+
+
+// textStyles
+const TextStyle mainHeading = TextStyle(fontSize: 20, fontWeight: FontWeight.w600, color: Color(0xff3C4143));
+const TextStyle subHeading = TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Color(0xff3C4143));
+const TextStyle heading = TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: Color(0xff3C4143));
+const TextStyle text = TextStyle(fontSize: 12, fontWeight: FontWeight.w400, color: Color(0xff7C8385));
